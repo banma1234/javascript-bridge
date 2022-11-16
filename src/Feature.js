13b60c562e7a58ef73093331d05c27f7dbdece92
banma70 <banma1234@gmail.com>
2022-11-16 17:37:10 +0900

    feature: 다리크기 사용자로부터 입력받기

--- src/Feature.js
+++ src/Feature.js
@@ -1,7 +1,21 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const BridgeMaker = require("../src/BridgeMaker");
+const MakeRandomValue = require('../src/BridgeRandomNumberGenerator');
 
 class Feature {
     play() {
-        
+        let bridge = this.inputUserValue();
+    }
+
+    inputUserValue() {
+        let bridge = [];
+        MissionUtils.Console.readline("다리크기 입력.", (INPUT) => {
+            bridge = BridgeMaker.makeBridge(INPUT, MakeRandomValue.generate());
+        })
+
+        return bridge;
     }
 }
+
+// const app = new Feature();
+// app.play();
