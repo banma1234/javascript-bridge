a4d4212b6a64d5ffb651c7bede99516ce0ee5cd6
banma70 <banma1234@gmail.com>
2022-11-20 16:42:33 +0900

    feature: 사용자로부터 건널 다리의 칸 입력받는 함수

--- src/Feature.js
+++ src/Feature.js
@@ -4,16 +4,33 @@ const MakeRandomValue = require('../src/BridgeRandomNumberGenerator');
 
 class Feature {
     play() {
-        let bridge = this.inputUserValue();
+        let bridge = this.inputBridge();
+        let userMove = this.inputUserMove();
     }
 
-    inputUserValue() {
+    inputBridge() {
         let bridge = [];
         MissionUtils.Console.readline("다리크기 입력.", (INPUT) => {
             bridge = BridgeMaker.makeBridge(INPUT, MakeRandomValue.generate());
         })
 
         return bridge;
+    }
+
+    inputUserMove() {
+        let result = "";
+        MissionUtils.Console.readline("이동할 칸 선택.", (INPUT) => {
+            this.inputUserMoveError(INPUT);
+            result = INPUT;
+        })
+
+        return result;
+    }
+
+    inputUserMoveError(INPUT) {
+        if (INPUT != "U" | INPUT != "D") {
+            throw new Error("[Error] U 혹은 D를 입력해주세요.")
+        }
     }
 }
 
